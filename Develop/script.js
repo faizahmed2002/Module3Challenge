6eca63c54c1f322c9791416805e20b0f7a316728
faizahmed2002 <faizahmed2002@gmail.com>
2022-10-13 20:58:01 +0100

    Addednumbersspecialcharlowercaseandcapitalletters

--- Develop/script.js
+++ Develop/script.js
@@ -2,18 +2,23 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var userinput = window.prompt("Random Password")
+  var userinput = window.prompt("How long do you want your password to be?")
   var PasswordLength = parseInt(userinput)
 
   if(isNaN(PasswordLength)) {
-    window.alert("That is not a number")
+    window.alert("That is not a number!")
     return
   } 
 
-  
-  
-  
+  if (PasswordLength < 8 || PasswordLength > 128 ) {
+    window.alert("password length must be between 8 and 128 characters")
+    return  
+  }
 
+  var userWantsNumbers = window.confirm("Would you like to include numbers in your password?")
+  var userWantsSpecialCharacters = window.confirm("Would you like to include special characters in your password?")
+  var userWantsLowerCase = window.confirm("Would you like to include lower case in your password?")
+  var userWantsCapitalLetters = window.confirm("Would you like to include capital letters in your password?")
   
 }
 
