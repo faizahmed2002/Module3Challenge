25f6e197043954bcb65929c6eb9ac3eb126afe55
faizahmed2002 <faizahmed2002@gmail.com>
2022-10-13 21:10:10 +0100

    addedlistwithallvalues

--- Develop/script.js
+++ Develop/script.js
@@ -19,6 +19,19 @@ function generatePassword() {
   var userWantsSpecialCharacters = window.confirm("Would you like to include special characters in your password?")
   var userWantsLowerCase = window.confirm("Would you like to include lower case in your password?")
   var userWantsCapitalLetters = window.confirm("Would you like to include capital letters in your password?")
+
+// ^ These are true and false values 
+
+var numberlist = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+var symbollist = ("!", "_", "+", "@", "#", "~")
+var lowercaselist = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+var capitalletterslist = []
+
+for (var i = 0; i < lowercaselist.length; i++) {
+  capitalletterslist[i] = lowercaselist[i].toLocaleUpperCase()
+}
+
+
   
 }
 
